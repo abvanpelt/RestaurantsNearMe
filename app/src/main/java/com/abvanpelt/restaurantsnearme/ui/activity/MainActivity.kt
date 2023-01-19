95bdb6808ba54a08804f2345ccc2b3ad4424174d
Allison Van Pelt <avantpelt@gmail.com>
2023-01-19 14:29:37 -0700

    Add ACCESS_COARSE_LOCATION permission request to Activity

--- app/src/main/java/com/abvanpelt/restaurantsnearme/ui/activity/MainActivity.kt
+++ app/src/main/java/com/abvanpelt/restaurantsnearme/ui/activity/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.abvanpelt.restaurantsnearme.ui.activity
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.abvanpelt.restaurantsnearme.R
 import com.abvanpelt.restaurantsnearme.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayoutMediator
 
 const val INDEX_FRAGMENT_MAP = 0
@@ -31,5 +37,55 @@ class MainActivity : AppCompatActivity() {
         }
 
         setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        checkLocationPermission()
+    }
+
+    /*
+     * Check whether or not we were already granted location permission from the user.
+     * If not, request the permission.
+     */
+    private fun checkLocationPermission() {
+        val request = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            // TODO forward the information to the ViewModel
+        }
+
+        when {
+            hasLocationPermission() -> {
+                // TODO forward the information to the ViewModel
+            }
+            showPermissionExplanation() -> {
+                Snackbar
+                    .make(binding.root, R.string.text_permission, Snackbar.LENGTH_INDEFINITE)
+                    .setAction(R.string.text_ok) {
+                        request.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
+                    }
+                    .show()
+            }
+            else -> request.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
+        }
+    }
+
+    /*
+     * Returns true if the user has granted location permission to the app.
+     */
+    private fun hasLocationPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    /*
+     * Returns true if we should display a rationale for the location permission request.
+     */
+    private fun showPermissionExplanation(): Boolean {
+        return ActivityCompat.shouldShowRequestPermissionRationale(
+            this,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
     }
 }
