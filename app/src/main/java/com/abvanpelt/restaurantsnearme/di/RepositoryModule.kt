9a00e3ea24b60ecc0bf6b4ae0f643ed9b8a6fd16
Allison Van Pelt <avantpelt@gmail.com>
2023-01-22 18:31:00 -0700

    Delete PermissionsRepository

--- app/src/main/java/com/abvanpelt/restaurantsnearme/di/RepositoryModule.kt
+++ app/src/main/java/com/abvanpelt/restaurantsnearme/di/RepositoryModule.kt
@@ -1,20 +1,10 @@
 package com.abvanpelt.restaurantsnearme.di
 
-import com.abvanpelt.restaurantsnearme.repository.PermissionsRepository
-import com.abvanpelt.restaurantsnearme.repository.PermissionsRepositoryImpl
-import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
-
-    @Singleton
-    @Binds
-    abstract fun bindPermissionsRepository(
-        permissionsRepositoryImpl: PermissionsRepositoryImpl
-    ): PermissionsRepository
 }
--- app/src/main/java/com/abvanpelt/restaurantsnearme/ui/activity/MainViewModel.kt
+++ app/src/main/java/com/abvanpelt/restaurantsnearme/ui/activity/MainViewModel.kt
@@ -1,16 +1,13 @@
 package com.abvanpelt.restaurantsnearme.ui.activity
 
 import androidx.lifecycle.ViewModel
-import com.abvanpelt.restaurantsnearme.repository.PermissionsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(
-    private val permissionsRepository: PermissionsRepository
-): ViewModel() {
+class MainViewModel @Inject constructor(): ViewModel() {
 
     fun onPermissionResult(hasLocationPermission: Boolean) {
-        permissionsRepository.setLocationPermission(hasLocationPermission)
+        // TODO
     }
 }
